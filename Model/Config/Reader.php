2bbdfa0553730faec548ce5bf0c6b1ff9b36a621
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2017-06-24 14:40:48 +0200

    🖖 Add configuration reader

---
+++ Model/Config/Reader.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Opengento\FeatureToggle2\Model\Config;
+
+use Magento\Framework\Config\Converter;
+use Magento\Framework\Config\FileResolverInterface;
+use Magento\Framework\Config\Reader\Filesystem;
+use Magento\Framework\Config\SchemaLocator;
+use Magento\Framework\Config\ValidationStateInterface;
+
+class Reader extends Filesystem
+{
+
+    /**
+     * @param FileResolverInterface $fileResolver
+     * @param Converter $converter
+     * @param SchemaLocator $schemaLocator
+     * @param ValidationStateInterface $validationState
+     * @param string $fileName
+     * @param array $idAttributes
+     * @param string $domDocumentClass
+     * @param string $defaultScope
+     */
+    public function __construct(
+        FileResolverInterface $fileResolver,
+        Converter $converter,
+        SchemaLocator $schemaLocator,
+        ValidationStateInterface $validationState,
+        $fileName = 'toggles.xml',
+        $idAttributes = [],
+        $domDocumentClass = 'Magento\Framework\Config\Dom',
+        $defaultScope = 'global'
+    ) {
+        parent::__construct(
+            $fileResolver,
+            $converter,
+            $schemaLocator,
+            $validationState,
+            $fileName,
+            $idAttributes,
+            $domDocumentClass,
+            $defaultScope
+        );
+    }
+
+}
+
