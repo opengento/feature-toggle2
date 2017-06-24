fc54467c13de867edd7c900b88e9a959f86ab6d1
Vincent MARMIESSE <vincent.marmiesse@ph2m.com>
2017-06-24 13:06:48 +0200

    🎉 Init module

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © 2017 Opengento - All rights reserved.
+ * See LICENSE.md bundled with this module for license details.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Opengento_FeatureToggle2',
+    __DIR__
+);
